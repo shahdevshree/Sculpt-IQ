a5f93bda03168ed423e63c2e9b017b2b07d3dbd1
Devshree <shahdevshree03@gmail.com>
2025-05-07 14:09:38 -0400

    Removed unused imports

--- src/components/UserPrograms.tsx
+++ src/components/UserPrograms.tsx
@@ -1,16 +1,9 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import {
-  ChevronRight,
-  Dumbbell,
   Sparkles,
-  Users,
-  Clock,
-  AppleIcon,
-  ShieldIcon,
 } from "lucide-react";
-import { USER_PROGRAMS } from "@/constants";
+
 
 const UserPrograms = () => {
   return (
